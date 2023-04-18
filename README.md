8bf977d5a6a8456b26d1ebadff82f66abc67f8a0
Daniel Koirala <51556023+DanielKoirala@users.noreply.github.com>
2023-04-17 23:59:41 -0400

    multiplayer functionality

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Pixel_Painter_prototype
- 
+
+instructions:
+
+1.  install node (nodejs.org)
+2.  in vscode terminal:
+    npm init
+    npm install express --save
+    npm install socket.io --save
+
+3.  in vscode terminal:
+    node server.js
+4.  in browser:
+    localhost:3000
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(3000);
+
+app.use(express.static("public"));
+
+console.log("server run");
+
+var socket = require("socket.io");
+
+var io = socket(server, {
+  cors: {
+    origin: "http://localost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.sockets.on("connection", newConnection);
+
+function newConnection(socket) {
+  console.log("new connection:" + socket.id);
+  socket.on("pixel", pixelMsg);
+
+  function pixelMsg(data) {
+    io.sockets.emit("pixel", data);
+    //socket.broadcast.emit("pixel", data);
+    console.log(data);
+  }
+}
